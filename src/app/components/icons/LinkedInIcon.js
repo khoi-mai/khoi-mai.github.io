c5cd2c9c48310ddde0c7172f0fe92ff7f36b4752
khoi-mai <khoi.m1204@gmail.com>
2026-04-03 03:46:23 -0400

    Create LinkedInIcon.js

---
+++ src/app/components/icons/LinkedInIcon.js
@@ -0,0 +1,12 @@
+export default function LinkedInIcon({ className }) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            className={className}
+        >
+            <path d="M4.98 3.5C4.98 4.88 3.88 6 2.5 6S0 4.88 0 3.5 1.12 1 2.5 1 4.98 2.12 4.98 3.5zM0 8h5v16H0V8zm7.5 0h4.8v2.2h.1c.7-1.3 2.4-2.6 5-2.6 5.4 0 6.4 3.5 6.4 8V24h-5v-7.5c0-1.8 0-4.1-2.5-4.1-2.5 0-2.9 2-2.9 4V24h-5V8z"/>
+        </svg>
+    );
+}
