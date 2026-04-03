1c36944f659050d9c62883c727287c9fba4360f1
khoi-mai <khoi.m1204@gmail.com>
2026-04-03 03:46:41 -0400

    Create page.js

---
+++ src/app/compositing/page.js
@@ -0,0 +1,21 @@
+"use client";
+import ProjectsList from "../components/ProjectsList";
+
+const compositingProjects = [
+  {
+    title: "Various projects",
+    description: "Composited in Nuke, After Effects, Fusion, and Harmony",
+    tags: ["Nuke", "VFX"],
+    videoUrl: "https://player.vimeo.com/video/697168217",
+  },
+  {
+    title: "Little Demon",
+    description: "Composited in Harmony",
+    tags: ["Nuke", "VFX", "Color"],
+    videoUrl: "https://player.vimeo.com/video/1084766763",
+  },
+];
+
+export default function CompositingPage() {
+  return <ProjectsList projectList={compositingProjects} />;
+}
