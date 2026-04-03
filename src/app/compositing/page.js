a62dfae0d07618e51aa8e1c94ad52f3e691f04e3
khoi-mai <khoi.m1204@gmail.com>
2026-04-03 03:57:48 -0400

    Update page.js

--- src/app/compositing/page.js
+++ src/app/compositing/page.js
@@ -4,18 +4,18 @@ import ProjectsList from "../components/ProjectsList";
 const compositingProjects = [
   {
     title: "Various projects",
-    description: "2D, 3D, Live action",
-    tags: ["Nuke", "After Effects", "Fusion", "Harmony"],
+    description: "Composited in Nuke, After Effects, Fusion, and Harmony",
+    tags: ["Nuke", "VFX"],
     videoUrl: "https://player.vimeo.com/video/697168217",
   },
   {
     title: "Little Demon",
-    description: "2D",
-    tags: ["Harmony"],
+    description: "Composited in Harmony",
+    tags: ["Harmony", "Color", "VFX"],
     videoUrl: "https://player.vimeo.com/video/1084766763",
   },
 ];
 
 export default function CompositingPage() {
-  return <ProjectsList projectList={compositingProjects || []} />;
+  return <ProjectsList projectList={compositingProjects} />;
 }
