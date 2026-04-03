94e2c9b96e38e468908cff9ed80ae2fa322439b5
khoi-mai <khoi.m1204@gmail.com>
2026-04-03 03:46:26 -0400

    Create IMDBIcon.js

---
+++ src/app/components/icons/IMDBIcon.js
@@ -0,0 +1,12 @@
+export default function IMDBIcon({ className }) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            className={className}
+        >
+            <path d="M2 2h20v20H2V2zm1 1v18h18V3H3zm2 3h2v2H5V6zm3 0h2v2H8V6zm3 0h2v2h-2V6zm3 0h2v2h-2V6zm3 0h2v2h-2V6zM5 9h2v2H5V9zm3 0h2v2H8V9zm3 0h2v2h-2V9zm3 0h2v2h-2V9zm3 0h2v2h-2V9z"/>
+        </svg>
+    );
+}
