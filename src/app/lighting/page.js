d554bf1b5d9cb747a0baf7ff9e5eefbcdfc5c4d6
khoi-mai <khoi.m1204@gmail.com>
2026-04-03 03:46:47 -0400

    Create page.js

---
+++ src/app/lighting/page.js
@@ -0,0 +1,15 @@
+"use client";
+import ProjectsList from "../components/ProjectsList";
+
+const lightingProjects = [
+  {
+    title: "Various Barbie projects",
+    description: "Lighting, Rendering, and Compositing in Katana and Nuke",
+    tags: ["Maya", "Arnold", "Lighting"],
+    videoUrl: "https://player.vimeo.com/video/875762647",
+  },
+];
+
+export default function LightingPage() {
+  return <ProjectsList projectList={LightingProjects} />;
+}
